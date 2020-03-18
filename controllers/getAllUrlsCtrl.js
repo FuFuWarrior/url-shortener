2b2865e06821cf0ea7846fdeda6c309653ca7889
FuFuWarrior <akpuwarrior@gmail.com>
2020-03-19 00:23:36 +0100

    changed path / controller

--- controllers/getAllUrlsCtrl.js
+++ controllers/getAllUrlsCtrl.js
@@ -3,11 +3,12 @@ const db = require('../models/database')
 const {pool} = db;
 
 exports.getAllUrls = (req,res) => {
+  res.send('HELLO NIGERIA')
     pool.query('SELECT * FROM URLS')
     .then((results) => {
-        const shortUrls = results.rows 
+        const shortUrls = results.rows
         res.render('index', {shortUrls: shortUrls})
     })
     .catch((e) => console.log(e))
-    
+
 }
