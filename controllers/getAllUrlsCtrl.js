1fc89232d3eaf2780086877bac4397a866f24cf9
FuFuWarrior <akpuwarrior@gmail.com>
2020-03-19 00:28:18 +0100

    added somtehing

--- controllers/getAllUrlsCtrl.js
+++ controllers/getAllUrlsCtrl.js
@@ -3,12 +3,11 @@ const db = require('../models/database')
 const {pool} = db;
 
 exports.getAllUrls = (req,res) => {
-  res.send('HELLO NIGERIA')
-    pool.query('SELECT * FROM URLS')
-    .then((results) => {
-        const shortUrls = results.rows
-        res.render('index', {shortUrls: shortUrls})
-    })
-    .catch((e) => console.log(e))
-
+  res.render('index')
+    // pool.query('SELECT * FROM URLS')
+    // .then((results) => {
+    //     const shortUrls = results.rows
+    //     res.render('index', {shortUrls: shortUrls})
+    // })
+    // .catch((e) => console.log(e))
 }
